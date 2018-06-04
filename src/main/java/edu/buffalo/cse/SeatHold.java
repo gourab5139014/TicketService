db2c83280f02058ae99def203c44e39ac5a886a2
gourab <#Gourab123>
2018-06-03 23:50:27 -0400

    Changed SeatStatus to a Seat object

--- src/main/java/edu/buffalo/cse/SeatHold.java
+++ src/main/java/edu/buffalo/cse/SeatHold.java
@@ -5,8 +5,19 @@ import java.time.Instant;
 public class SeatHold {
     Instant heldAt;
     String customerEmail;
+    int rowIndex;
+    int columnIndex;
+    int numberofSeats;
 
     public SeatHold(){
         this.heldAt = Instant.now();
+    }
+
+    public String release(){
+        return null;
+    }
+
+    public void reserve(){
+        
     }
 }
